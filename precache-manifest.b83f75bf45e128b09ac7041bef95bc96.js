a278a63a44e96212b081580e85c9887235acbc50
Manuel <Manuelasomc@gmail.com>
2020-07-14 13:25:50 +0100

    Updates

---
+++ precache-manifest.b83f75bf45e128b09ac7041bef95bc96.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2329719388dbf006beaeb06e18ae5f",
+    "url": "/pokedex2.0/index.html"
+  },
+  {
+    "revision": "c589a8e1c9a7f37b406a",
+    "url": "/pokedex2.0/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "afeb568e584233295b7d",
+    "url": "/pokedex2.0/static/css/main.b835a1a0.chunk.css"
+  },
+  {
+    "revision": "c589a8e1c9a7f37b406a",
+    "url": "/pokedex2.0/static/js/2.08998ace.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/pokedex2.0/static/js/2.08998ace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afeb568e584233295b7d",
+    "url": "/pokedex2.0/static/js/main.7b97f00c.chunk.js"
+  },
+  {
+    "revision": "2bebba0c2ac977b6899e",
+    "url": "/pokedex2.0/static/js/runtime-main.298fb79f.js"
+  },
+  {
+    "revision": "78745eb1af18d9a1902208f513b89487",
+    "url": "/pokedex2.0/static/media/backgroundpokemon.78745eb1.png"
+  },
+  {
+    "revision": "7e45b413c8eea46ac0e5dd3d571c5d84",
+    "url": "/pokedex2.0/static/media/pokebackground.7e45b413.jpg"
+  }
+]);
